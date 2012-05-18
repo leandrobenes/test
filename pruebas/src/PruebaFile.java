82131d49724b5ba3cde64b833af7020f1df29d87
Leandro Benes <lbenes@ubuntu>
2012-05-18 09:27:27 -0300

    prueba sobre branch test

--- pruebas/src/PruebaFile.java
+++ pruebas/src/PruebaFile.java
@@ -3,7 +3,7 @@ public class PruebaFile {
 
 	public static void main(String[] args) {
 		
-		System.out.println("probando sobre branch issue 2");
+		System.out.println("probando sobre branch test 1");
 	}
 	
 }
