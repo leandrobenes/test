2e03586bfe1c6da6d4f33318c0aa4072cb63e823
Leandro Benes <lbenes@ubuntu>
2012-06-06 15:00:08 -0300

    commiting testflow changes

--- pruebas/src/PruebaFile.java
+++ pruebas/src/PruebaFile.java
@@ -3,7 +3,7 @@ public class PruebaFile {
 
 	public static void main(String[] args) {
 		
-		System.out.println("probando sobre branch issue");
+		System.out.println("probando sobre branch issue 222");
 	}
 	
 }
